e22818d2647aa57f3cef324c7c1184b17367855c
achingbrain <alex@achingbrain.net>
2019-01-29 16:16:28 +0000

    add hash to filename

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const config = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name]-[contenthash].js',
     publicPath: '/'
   },
   plugins: [
